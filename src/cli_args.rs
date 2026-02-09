4cc73bc7eb825f79487351eeb27d6cd72dd3600d
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-09 10:17:19 +0100

    Changed the functions return type
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -2,16 +2,16 @@ use std::error::Error;
 use std::env;
 
 
-pub fn get_url() -> Result<(), Box<dyn Error>> {
+pub fn get_url() -> Result<String, Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         return Err(format!("No arguments/ URL were provided").into());
     }
 
-    let _user_input = &args[1];
+    let user_input = args[1].clone();
 
-    Ok(())
+    Ok(user_input)
 
 }
 
