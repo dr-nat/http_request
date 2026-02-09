a9f86e7a3bb5ac97edc57f63d08de4d8a6343bdf
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-09 10:16:32 +0100

    Added the url crate and converted the cli argument to a url
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,11 @@
-use url::{Url, parser};
+use std::error::Error;
+use crate::cli_args;
+use url::*;
+
+pub fn parse_args() -> Result<Url, Box<dyn Error>>{
+    let args = cli_args::get_url(); 
+
+    let url = Url::parse(&args?.as_str())?;
+
+    Ok(url)
+}
