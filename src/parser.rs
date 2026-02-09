c5075f3e2a9fef9a038dc38277b69578a37e5b40
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-09 11:49:58 +0100

    Refactored the the parser module to have to functions and introduced test on the new methods
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -2,10 +2,44 @@ use std::error::Error;
 use crate::cli_args;
 use url::*;
 
-pub fn parse_args() -> Result<Url, Box<dyn Error>>{
-    let args = cli_args::get_url(); 
+pub fn parse_args(input: &str) -> Result<Url, Box<dyn Error>> {
 
-    let url = Url::parse(&args?.as_str())?;
+
+    let url = Url::parse(input)?;
+    
+    let _host = &url.host();
+
+    let _path = &url.path();
+
+    let _port = &url.port();
 
     Ok(url)
 }
+
+pub fn run_args() -> Result<Url, Box<dyn Error>> {
+    let link = cli_args::get_url()?;
+
+    parse_args(&link)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check() {
+
+        let link = "https://arstechnica.com";
+        
+        let result = parse_args(link);
+
+        assert!(result.is_ok());
+
+        let url = result.unwrap();
+
+        assert_eq!(url.host_str(), Some("arstechnica.com"));
+        //assert_eq!(url.path(), "https://gemini.google.com/app/10cca38cc51cc01f")
+    }
+
+}
+
