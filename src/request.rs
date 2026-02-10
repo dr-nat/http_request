de096dc11f98ba22cbc7f447cd2a88cdbcadbf92
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-10 14:06:45 +0100

    Created the tcp stream connection for sending request and recieving responses
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/request.rs
+++ src/request.rs
@@ -1,11 +1,25 @@
 use crate::parser;
+use std::net::TcpStream;
 use std::error::Error;
+use std::io::prelude::*;
 
 pub fn send_request() -> Result<(), Box<dyn Error>>{
-    let request = parser::run_args()?; 
+    let argument = parser::run_args()?; 
 
-    let _ = format!("GET {:?} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", request.host_str(), request.path());
+    let request = format!("GET {:?} HTTP/1.1\r\nHost: {}\r\n port: {:?}\r\nConnection: close\r\n\r\n", argument.host_str(), argument.path(), argument.port_or_known_default());
+    println!("{}", &request);
     
+    let  stream = TcpStream::connect(&request);
+    
+    let mut streamed_value = stream?;
+
+    streamed_value.write_all(&request.as_bytes())?;
+    
+    let mut response = String::new();
+
+   streamed_value.read_to_string(&mut response)?;
+
+   println!("The server returns: {:?}", response);
     
     Ok(())
 }
