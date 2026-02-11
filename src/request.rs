3b10d9e7797c7057b17532ccfd98388d42fd51bd
Nathaniel Parah <nathanielparah579@gmail.com>
2026-02-11 10:42:11 +0100

    Refactored code with comments
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/request.rs
+++ src/request.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 use std::io::prelude::*;
 
 pub fn send_request() -> Result<(), Box<dyn Error>>{
-    let argument = parser::run_args()?; 
+    // Called the parser::run_args function to accept the commandling args and convert it to a URL let argument = parser::run_args()?; 
 
     let host = argument.host_str().ok_or("No host found")?;
 
@@ -17,12 +17,18 @@ pub fn send_request() -> Result<(), Box<dyn Error>>{
     let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", path, host);
    
     println!("\nConnecting to address: |{}|", address);
+
+    // The idead behind connecting to the hosts server!
     let  stream = TcpStream::connect(&address);
     
+    // Unpacking the successful scenario of the connection!
     let mut streamed_value = stream?;
 
+    // The process of sending the entire address to the server!
     streamed_value.write_all(&request.as_bytes())?;
-    
+
+
+   // The process of storing the response in form of a buffer! 
     let mut response = String::new();
 
    streamed_value.read_to_string(&mut response)?;
